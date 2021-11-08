c2e7db6c7e4abebc550c484dcdab67b57065a227
jacob-branch <jacob.branch@kakaocorp.com>
2021-11-08 23:10:50 +0900

    house-utils init

---
+++ src/test/java/clean/code/houseutils/HouseUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package clean.code.houseutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
